af8ef83fa1f96518e90d44b0077f20ce1d7d5113
orkunkn <orkunkaymann@gmail.com>
2023-07-07 17:54:14 +0300

    Added missing controllers

--- src/main/java/com/project/questapp/repos/LikeRepository.java
+++ src/main/java/com/project/questapp/repos/LikeRepository.java
@@ -1,9 +1,16 @@
 package com.project.questapp.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.questapp.entities.Like;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
+	List<Like> findByUserIdAndPostId(Long userId, Long postId);
+
+	List<Like> findByUserId(Long userId);
+
+	List<Like> findByPostId(Long postId);
 }
